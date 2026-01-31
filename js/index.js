09b80e53bfe9a5482e36c6b33e2a900e1f264a8b
Gabriel Luan <gabrielassembleiano@gmail.com>
2026-01-31 12:20:40 -0300

    Atualizando a hora

--- js/index.js
+++ js/index.js
@@ -1,41 +1,48 @@
 //Selecionando os elementos
-const horas = document.querySelector('#hr');
-const minutos = document.querySelector('#min');
-const segundos = document.querySelector('#sec');
-
+const horas = document.querySelector("#hr");
+const minutos = document.querySelector("#min");
+const segundos = document.querySelector("#sec");
 
 const clock = function () {
+  setInterval(() => {
     let dt = new Date();
     let hr = dt.getHours();
     let min = dt.getMinutes();
 
     // Formata para 2 dígitos
-    hr = hr < 10 ? '0' + hr : hr;
-    min = min < 10 ? '0' + min : min;
+    hr = hr < 10 ? "0" + hr : hr;
+    min = min < 10 ? "0" + min : min;
 
     horas.textContent = hr;
     minutos.textContent = min;
-}
+  },1000);
+};
 
 const data = function () {
-    let dt = new Date();
-    const month = [
-        'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
-        'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
-    ];
+  let dt = new Date();
+  const month = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ];
 
-    let dataCalendario = document.querySelector('.data');
-    let dateDay = dt.getDate(); // Dia do mês
-    let dateMonth = dt.getMonth();
-    let dateFull = dt.getFullYear();
+  let dataCalendario = document.querySelector(".data");
+  let dateDay = dt.getDate(); // Dia do mês
+  let dateMonth = dt.getMonth();
+  let dateFull = dt.getFullYear();
 
-    dataCalendario.innerHTML = `Dia ${dateDay} de ${month[dateMonth]} de ${dateFull}`;
-}
-
+  dataCalendario.innerHTML = `Dia ${dateDay} de ${month[dateMonth]} de ${dateFull}`;
+};
 
 //Funções sendo executadas...
 clock();
 data();
-
-
-
